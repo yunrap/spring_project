e4603200c9c6714c5f84ce8d537eaac8419b1f7c
yunrap <dbsdpwl7@gmail.com>
2023-09-02 18:28:57 +0900

    프로젝트 init

---
+++ src/main/java/com/example/spring_basic_project/SpringBasicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_basic_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_basic_project/SpringBasicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_basic_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
